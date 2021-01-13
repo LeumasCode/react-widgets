46c1fc65b94b5cd0efc338b54cf7681949a12d2b
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-13 06:38:47 -0500

    input with hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from './components/Search'
 
 const items = [
   { title: "what is react", content: "React is a javascript framework" },
@@ -16,7 +17,7 @@ const items = [
 const App = () => {
   return (
     <div>
-      <Accordion items={items} />
+      <Search/>
     </div>
   );
 };
