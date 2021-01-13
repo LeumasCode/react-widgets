a7fd5e174136826adc2c57b95ff5faaedc6f5f7a
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-13 11:12:59 -0500

    using debounce method

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,47 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+
+  
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
+
+
+  useEffect(() => {
+    const getSearch = async () => {
+      try {
+        const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+          params: {
+            action: "query",
+            list: "search",
+            srsearch: debouncedTerm,
+            origin: "*",
+            format: "json",
+          },
+        });
+
+        setResults(data.query.search);
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
+
+    getSearch();
+
+    return () => {};
+  }, [debouncedTerm]);
+
+ 
 
   const renderedResults = results.map((result) => {
     return (
@@ -23,40 +63,6 @@ const Search = () => {
       </div>
     );
   });
-
-  useEffect(() => {
-    const getSearch = async () => {
-      try {
-        const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
-          params: {
-            action: "query",
-            list: "search",
-            srsearch: term,
-            origin: "*",
-            format: "json",
-          },
-        });
-
-        setResults(data.query.search);
-      } catch (err) {
-        console.error(err.message);
-      }
-    };
-
-    if (term && !results.length) {
-      getSearch();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          getSearch();
-        }
-      }, 1000);
-
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [term, results]);
 
   return (
     <div>
