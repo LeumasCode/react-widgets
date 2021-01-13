db7d8cab8a3ae1742ad018eaebef0b78eebd8f73
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-13 08:39:36 -0500

    fetching data

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
+
+
+  const getSearch = async () => {
+    try {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          srsearch: term,
+          origin: '*',
+          format: "json",
+        },
+      });
+
+      setResults(data.query.search)
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+
+  useEffect(() => {
+    getSearch()
+  });
+
   return (
     <div>
       <div className="ui form">
