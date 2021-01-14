13f17b9c58f6bf2e3b0011dd1e0fd1e37c12dcbd
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-14 09:12:30 -0500

    building convert component

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Convert from "./Convert";
 import Dropdown from "./Dropdown";
 
 const options = [
@@ -29,7 +30,9 @@ const Translate = () => {
         selected={language}
         onSelectedChange={setLanguage}
       />
-      {text}
+      <hr/>
+      <h3 className="ui header">Output</h3>
+      <Convert language={language} text={text}/>
     </div>
   );
 };
