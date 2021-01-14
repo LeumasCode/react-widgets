c82043dfce725b53f5b412fdf94abed1754c3a9e
Opeyemi Samuel <opeyemisamuel222@gmail.com>
2021-01-14 09:21:14 -0500

    using google api

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,19 +1,22 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Convert = ({language, text}) => {
+const Convert = ({ language, text }) => {
+  useEffect(() => {
+    axios.post(
+      "https://translation.googleapis.com/language/translate/v2",
+      {},
+      {
+        params: {
+          q: text,
+          target: language.value,
+          key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+        },
+      }
+    );
+  }, [language, text]);
 
+  return <div></div>;
+};
 
-
-useEffect(() => {
-    
-    console.log('new language and text');
-}, [language, text])
-
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default Convert
+export default Convert;
